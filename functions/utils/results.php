c436c540ba787405625eb02f139a1fd9db7db38c
Julian <dussollier.julian@gmail.com>
2023-11-01 16:14:11 -0400

    result form for treatment

---
+++ functions/utils/results.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * La page résults va permettre de faire le traitement 
+ * de tous les formulaires il faut donc que je sache 
+ * dans quel formulaire je suis pour en faire le traitement
+  */
+  
+
+?>
